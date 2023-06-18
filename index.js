02ca6e55b47e046b6aca112cdc91cb50ad726f63
Ryan Heitmann <ryan.heitmann@gmail.com>
2023-06-18 21:00:04 +1200

    setting PORT

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ let persons = [
     }
 ]
 
-const PORT = process.env || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
