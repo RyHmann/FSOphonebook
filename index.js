9200c4bfc492b9b16ba45e14fb7ca1cb6cd8b690
Ryan Heitmann <ryan.heitmann@gmail.com>
2023-06-23 19:07:27 +1200

    Adding eslint, refactor error handling

--- index.js
+++ index.js
@@ -8,98 +8,89 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
+morgan.token('body', function (req) {return JSON.stringify(req.body)})
 app.use(morgan('tiny'))
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
-app.get('/info', (request, response) => {
-    var dateTime = new Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${dateTime}</p>`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 app.get('/api/persons', (request, response) => {
-    Contact.find({}).then(contact => {
-        if (contact) {
-            response.json(contact)
-        } else {
-            response.status(404).end()
-        }
-    })
+  Contact.find({}).then(contact => {
+    if (contact) {
+      response.json(contact)
+    } else {
+      response.status(404).end()
+    }
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Contact
-        .findById(request.params.id)
-        .then(contact => {
-            if (contact) {
-                response.json(contact)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch((error) => {
-            error => next(error)
-        })
+  Contact
+    .findById(request.params.id)
+    .then(contact => {
+      if (contact) {
+        response.json(contact)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Contact
-        .findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch((error) => {
-            error => next(error)
-        })
+  Contact
+    .findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
+morgan.token('body', function (req) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = new Contact({
-        name: body.name,
-        number: body.number
-    })
+  const person = new Contact({
+    name: body.name,
+    number: body.number
+  })
 
-    person.save()
-        .then(savedPerson => response.json(savedPerson))
-        .catch(error => next(error))
+  person.save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    const person = new Contact({
-        _id: request.params.id,
-        name: body.name,
-        number: body.number
-    })
-    
-    Contact.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'})
-           .then(result => response.json(result))
-           .catch(error => next(error))
+  const body = request.body
+  const person = new Contact({
+    _id: request.params.id,
+    name: body.name,
+    number: body.number
+  })
+
+  Contact.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(result => response.json(result))
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id'})
-    }
-    else if (error.name === 'ValidationError') {
-        console.log('error hit: ', error.message)
-        return response.status(400).json({error: error.message})
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    console.log('error hit: ', error.message)
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 app.use(errorHandler)
--- models/contact.js
+++ models/contact.js
@@ -4,47 +4,47 @@ const url = process.env.MONGODB_URI
 console.log('connecting to ', url)
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 
 const numberFormatValidator = (val) => {
-    let reg = /^\d{2,3}[-]\d+$/
-    return reg.test(val)
+  let reg = /^\d{2,3}[-]\d+$/
+  return reg.test(val)
 }
 
 const numberLengthValidator = (val) => {
-    return val.length  > 7
+  return val.length  > 7
 }
 
 const phoneNumberValidator = [
-    {validator: numberFormatValidator, message: 'Phone number requires a dash or hyphen'},
-    {validator: numberLengthValidator, message: 'Phone number must be at least 8 digits long'}
+  { validator: numberFormatValidator, message: 'Phone number requires a dash or hyphen' },
+  { validator: numberLengthValidator, message: 'Phone number must be at least 8 digits long' }
 ]
 
 
 const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        validate: phoneNumberValidator
-    }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: phoneNumberValidator
+  }
 })
 
 contactSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Contact', contactSchema)
